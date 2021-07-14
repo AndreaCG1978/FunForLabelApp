9c8a15499ddaee271de105eda29f096a60a33501
AndreaCG1978 <44733992+AndreaCG1978@users.noreply.github.com>
2021-07-14 13:03:28 -0300

    Delete config.php

--- phps/config.php
+++
@@ -1,9 +0,0 @@
-    <?php
-    $db = [
-        'host' => 'localhost',
-	'port' => '3306',
-        'username' => 'ggrassano',
-        'password' => 'd3H237e3',
-        'db' => 'funforlabels_test' 
-    ];
-    ?>
